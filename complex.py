0924d2a5e00ec14e2b4b9ed1eea9f736ee981ce0
pemix09 <pemix09@onet.pl>
2022-04-07 19:49:16 +0200

    init

--- complex.py
+++ complex.py
@@ -1,1 +1,17 @@
-print("Hello python!")
+class ComplexNumber:
+
+    real = 0
+    imagine = 0
+
+    def __init__(self, _real, _imagine):
+        
+        self.real = _real
+        self.imagine = _imagine
+
+    def Print(self):
+
+        print("Number: "+ str(self.real) +"+"+ str(self.imagine) + "i")
+
+
+number = ComplexNumber(12, 2)
+number.Print()
